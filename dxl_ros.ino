f9afbc2cc7228976d7535546a1827a443120b643
Davis Landry <davislandry1@gmail.com>
2019-11-14 18:02:05 -0700

    added speed control

--- dxl_ros.ino
+++ dxl_ros.ino
@@ -8,6 +8,8 @@
 #define ADDR_PRO_TORQUE_ENABLE          24                 // Control table address is different in Dynamixel model
 #define ADDR_PRO_GOAL_POSITION          30
 #define ADDR_PRO_PRESENT_POSITION       36
+#define ADDR_PRO_MOVING_SPEED           32
+#define ADDR_PRO_PRESENT_SPEED          38
 
 // Protocol version
 #define PROTOCOL_VERSION                1.0                 // See which protocol version is used in the Dynamixel
@@ -30,35 +32,59 @@
 
 void onPanPos(const std_msgs::Int16 &pos);
 void onTiltPos(const std_msgs::Int16 &pos);
+void onPanSpeed(const std_msgs::Int16 &sp);
+void onTiltSpeed(const std_msgs::Int16 &sp);
 
 //ROS integration
 
 ros::NodeHandle nh;
 std_msgs::Int16 dyna1_pos;
 std_msgs::Int16 dyna2_pos;
+std_msgs::Int16 dyna1_speed;
+std_msgs::Int16 dyna2_speed;
 ros::Publisher panPos("pan_cur_position", &dyna1_pos);
 ros::Publisher tiltPos("tilt_cur_position", &dyna2_pos);
+ros::Publisher panSpeed("pan_cur_speed", &dyna1_speed);
+ros::Publisher tiltSpeed("tilt_cur_speed", &dyna2_speed);
 ros::Subscriber<std_msgs::Int16> panGoal("pan_goal_position", &onPanPos);
 ros::Subscriber<std_msgs::Int16> tiltGoal("tilt_goal_position", &onTiltPos);
+ros::Subscriber<std_msgs::Int16> panSpeedGoal("pan_goal_speed", &onPanSpeed);
+ros::Subscriber<std_msgs::Int16> tiltSpeedGoal("tilt_goal_speed", &onTiltSpeed);
 
 
 char logBuffer[128];
-int pos1 = 2000, pos2 = 2000;
+int pos1 = 2000, pos2 = 2000, speed1 = 0, speed2 = 0;
 
 void onPanPos(const std_msgs::Int16 &pos) {
   pos1 = pos.data;
-  if(pos.data > 4095)
+  if (pos.data > 4095)
     pos1 = 4095;
-  if(pos.data < 0)
-    pos1 = 0; 
+  if (pos.data < 0)
+    pos1 = 0;
 }
 
 void onTiltPos(const std_msgs::Int16 &pos) {
   pos2 = pos.data;
-  if(pos.data > 3250)
+  if (pos.data > 3250)
     pos2 = 3250;
-  if(pos.data < 750)
+  if (pos.data < 750)
     pos2 = 750;
+}
+
+void onPanSpeed(const std_msgs::Int16 &sp){
+  speed1 = sp.data;
+  if(speed1 < 0)
+    speed1 = 0;
+  if(speed1 > 1023)
+    speed1 = 1023;
+}
+
+void onTiltSpeed(const std_msgs::Int16 &sp){
+  speed2 = sp.data;
+  if(speed2 < 0)
+    speed2 = 0;
+  if(speed2 > 1023)
+    speed2 = 1023;
 }
 
 void setup() {
@@ -67,8 +93,12 @@ void setup() {
   nh.initNode();
   nh.advertise(panPos);
   nh.advertise(tiltPos);
+  nh.advertise(panSpeed);
+  nh.advertise(tiltSpeed);
   nh.subscribe(panGoal);
   nh.subscribe(tiltGoal);
+  nh.subscribe(panSpeedGoal);
+  nh.subscribe(tiltSpeedGoal);
 
   pinMode(23, INPUT_PULLDOWN);
 
@@ -93,6 +123,8 @@ void setup() {
   uint8_t dxl_error = 0;                          // Dynamixel error
   int32_t dxl_present_position1 = 0;               // Present position
   int32_t dxl_present_position2 = 0;
+  int32_t dxl_present_speed1 = 0;
+  int32_t dxl_present_speed2 = 0;
 
   // Open port
   if (portHandler->openPort())
@@ -149,62 +181,25 @@ void setup() {
 
   while (1)
   {
-    if (digitalRead(23)) {
-      Serial.end();
-
-      
-      Serial.begin(BAUDRATE);
-      // Open port
-      if (portHandler->openPort())
-      {
-        nh.loginfo("Succeeded to open the port!\n");
-      }
-      else
-      {
-        nh.loginfo("Failed to open the port!\n");
-        nh.loginfo("Press any key to terminate...\n");
-        return;
-      }
-
-      // Set port baudrate
-      if (portHandler->setBaudRate(BAUDRATE))
-      {
-        nh.loginfo("Succeeded to change the baudrate!\n");
-      }
-      else
-      {
-        nh.loginfo("Failed to change the baudrate!\n");
-        nh.loginfo("Press any key to terminate...\n");
-        return;
-      }
-
-      // Enable Dynamixel Torque
-      dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, 1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
-      if (dxl_comm_result != COMM_SUCCESS)
-      {
-        packetHandler->getTxRxResult(dxl_comm_result);
-      }
-      else if (dxl_error != 0)
-      {
-        packetHandler->getRxPacketError(dxl_error);
-      }
-      else
-      {
-        nh.loginfo("Dynamixel 1 has been successfully connected \n");
-      }
-      dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, 2, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
-      if (dxl_comm_result != COMM_SUCCESS)
-      {
-        packetHandler->getTxRxResult(dxl_comm_result);
-      }
-      else if (dxl_error != 0)
-      {
-        packetHandler->getRxPacketError(dxl_error);
-      }
-      else
-      {
-        nh.loginfo("Dynamixel 2 has been successfully connected \n");
-      }
+    // Write goal speed 1
+    dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 1, ADDR_PRO_MOVING_SPEED, speed1, &dxl_error);
+    if (dxl_comm_result != COMM_SUCCESS)
+    {
+      packetHandler->getTxRxResult(dxl_comm_result);
+    }
+    else if (dxl_error != 0)
+    {
+      packetHandler->getRxPacketError(dxl_error);
+    }
+    // Write goal speed 2
+    dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 2, ADDR_PRO_MOVING_SPEED, speed2, &dxl_error);
+    if (dxl_comm_result != COMM_SUCCESS)
+    {
+      packetHandler->getTxRxResult(dxl_comm_result);
+    }
+    else if (dxl_error != 0)
+    {
+      packetHandler->getRxPacketError(dxl_error);
     }
     // Write goal position
     dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 1, ADDR_PRO_GOAL_POSITION, pos1, &dxl_error);
@@ -240,14 +235,6 @@ void setup() {
         packetHandler->getRxPacketError(dxl_error);
       }
 
-      //          nh.loginfo("[ID:1");
-      //          sprintf(logBuffer,"Goal Position: %d",pos1);
-      //          nh.loginfo(logBuffer);
-      //          sprintf(logBuffer,"Present Position: %d",dxl_present_position1);
-      //          nh.loginfo(logBuffer);
-
-
-
     } while ((abs(pos1 - dxl_present_position1) > DXL_MOVING_STATUS_THRESHOLD1));
     do
     {
@@ -262,19 +249,38 @@ void setup() {
         packetHandler->getRxPacketError(dxl_error);
       }
 
-      //         nh.loginfo("[ID:2");
-      //          sprintf(logBuffer,"Goal Position: %d",pos2);
-      //          nh.loginfo(logBuffer);
-      //          sprintf(logBuffer,"Present Position: %d",dxl_present_position2);
-      //          nh.loginfo(logBuffer);
-
     } while ((abs(pos2 - dxl_present_position2) > DXL_MOVING_STATUS_THRESHOLD2));
 
+    // Read present speed 1
+      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, 1, ADDR_PRO_PRESENT_SPEED, (uint32_t*)&dxl_present_speed1, &dxl_error);
+      if (dxl_comm_result != COMM_SUCCESS)
+      {
+        packetHandler->getTxRxResult(dxl_comm_result);
+      }
+      else if (dxl_error != 0)
+      {
+        packetHandler->getRxPacketError(dxl_error);
+      }
+      // Read present speed 2
+      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, 2, ADDR_PRO_PRESENT_SPEED, (uint32_t*)&dxl_present_speed2, &dxl_error);
+      if (dxl_comm_result != COMM_SUCCESS)
+      {
+        packetHandler->getTxRxResult(dxl_comm_result);
+      }
+      else if (dxl_error != 0)
+      {
+        packetHandler->getRxPacketError(dxl_error);
+      }
+    
     //ROS
     dyna1_pos.data = dxl_present_position1;
     dyna2_pos.data = dxl_present_position2;
+    dyna1_speed.data = dxl_present_speed1;
+    dyna2_speed.data = dxl_present_speed2;
     panPos.publish(&dyna1_pos);
     tiltPos.publish(&dyna2_pos);
+    panSpeed.publish(&dyna1_speed);
+    tiltSpeed.publish(&dyna2_speed);
     nh.spinOnce();
   }
 
